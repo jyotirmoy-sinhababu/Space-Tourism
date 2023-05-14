43ef26c5bc54000c2df7302cd7186c0bc2480641
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-14 14:01:41 +0530

    style added

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -7,26 +7,24 @@ const HomePage = () => {
   return (
     <div className='home-cnt'>
       <Nav />
-      <div className='home-txt-cnt'>
-        <div>
-          <div>
-            <p>SO, YOU WANT TO TRAVEL TO</p>
-            <h1>SPACE</h1>
+      <div className='home-main-info-cnt'>
+        <div className='home-txt-cnt'>
+          <div className='home-first-txt-cnt'>
+            <p className='home-intro-txt'>SO, YOU WANT TO TRAVEL TO</p>
+            <p className='home-heading'>SPACE</p>
           </div>
-          <div>
-            <p>
+          <div className='home-second-txt-cnt'>
+            <p className='home-second-txt-cnt-info'>
               Space Let’s face it; if you want to go to space, you might as well
               genuinely go to outer space and not hover kind of on the edge of
               it. Well sit back, and relax because we’ll give you a truly out of
               this world experience!
             </p>
           </div>
-          <div>
-            <p>EXPLORE </p>
-          </div>
         </div>
-        <div>
-          <p></p>
+
+        <div className='home-explore-cnt'>
+          <p className='explore-txt'>EXPLORE </p>
         </div>
       </div>
     </div>
