6d2efc9c1d2d8de48bdb6d475123edc32b3c7acc
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-15 13:04:06 +0530

    onclick event added to destinationCardBtn

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { Route, Routes } from 'react-router-dom';
 import HomePage from './pages/homePage/HomePage';
 import AllPageCnt from './pages/allPages/AllPageCnt';
 import DestinationPage from './pages/destination/DestinationPage';
+import ControlCount from './countState/ControlCount';
 
 function App() {
   return (
-    <div className='App'>
-      <Routes>
-        <Route path='/' element={<AllPageCnt />}>
-          <Route index element={<HomePage />} />
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/destination' element={<DestinationPage />} />
-        </Route>
-      </Routes>
-    </div>
+    <ControlCount>
+      <div className='App'>
+        <Routes>
+          <Route path='/' element={<AllPageCnt />}>
+            <Route index element={<HomePage />} />
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/destination' element={<DestinationPage />} />
+          </Route>
+        </Routes>
+      </div>
+    </ControlCount>
   );
 }
 
--- src/component/sub-component/DestinationCardBtn.jsx
+++ src/component/sub-component/DestinationCardBtn.jsx
@@ -1,12 +1,39 @@
 import React, { useContext } from 'react';
+import { countContext } from '../../countState/ControlCount';
 
 const DestinationCardBtn = () => {
+  const { countOne, countTwo, countThree, countZero } =
+    useContext(countContext);
   return (
     <div>
-      <button>MOON</button>
-      <button>MARS</button>
-      <button>EUROPA</button>
-      <button>TITAN</button>
+      <button
+        onClick={() => {
+          countZero();
+        }}
+      >
+        MOON
+      </button>
+      <button
+        onClick={() => {
+          countOne();
+        }}
+      >
+        MARS
+      </button>
+      <button
+        onClick={() => {
+          countTwo();
+        }}
+      >
+        EUROPA
+      </button>
+      <button
+        onClick={() => {
+          countThree();
+        }}
+      >
+        TITAN
+      </button>
     </div>
   );
 };
