a876ffbf8bfb09f24316bb0a742796182266155a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-05-15 13:27:15 +0530

    changes made

---
+++ src/component/cards/CrewListCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CrewListCard = () => {
+  return <div>CrewListCard</div>;
+};
+
+export default CrewListCard;
--- src/component/cards/DestinationCard.jsx
+++ src/component/cards/DestinationCard.jsx
@@ -9,65 +9,90 @@ const DestinationCard = ({}) => {
   const { count } = useContext(countContext);
 
   return (
-    <div>
-      <div className='destination-image'>
+    <div className='destination-cnt'>
+      <h3 className='destination-header'>01 PICK YOUR DESTINATION</h3>
+      <div className='destination-image-cnt'>
         {count == 0 && destinations ? (
-          <img src={destinations[0].images.png} alt='moon' />
+          <img
+            className='destination-image'
+            src={destinations[0].images.png}
+            alt='moon'
+          />
         ) : count == 1 && destinations ? (
-          <img src={destinations[1].images.png} alt='mars' />
+          <img
+            className='destination-image'
+            src={destinations[1].images.png}
+            alt='mars'
+          />
         ) : count == 2 && destinations ? (
-          <img src={destinations[2].images.png} alt='europa' />
+          <img
+            className='destination-image'
+            src={destinations[2].images.png}
+            alt='europa'
+          />
         ) : count == 3 && destinations ? (
-          <img src={destinations[2].images.png} alt='europa' />
+          <img
+            className='destination-image'
+            src={destinations[2].images.png}
+            alt='europa'
+          />
         ) : null}
       </div>
-      <div>
-        <div>
+      <div className='destination-txt-cnt'>
+        <div className='destination-btn-cnt'>
           <DestinationCardBtn />
         </div>
         {count == 0 && destinations ? (
-          <p>{destinations[0].name}</p>
+          <p className='destination-name'>{destinations[0].name}</p>
         ) : count == 1 && destinations ? (
-          <p>{destinations[1].name}</p>
+          <p className='destination-name'>{destinations[1].name}</p>
         ) : count == 2 && destinations ? (
-          <p>{destinations[2].name}</p>
+          <p className='destination-name'>{destinations[2].name}</p>
         ) : count == 3 && destinations ? (
-          <p>{destinations[3].name}</p>
+          <p className='destination-name'>{destinations[3].name}</p>
         ) : null}
         <div>
           {count == 0 && destinations ? (
-            <p>{destinations[0].description}</p>
+            <p className='destination-description'>
+              {destinations[0].description}
+            </p>
           ) : count == 1 && destinations ? (
-            <p>{destinations[1].description}</p>
+            <p className='destination-description'>
+              {destinations[1].description}
+            </p>
           ) : count == 2 && destinations ? (
-            <p>{destinations[2].description}</p>
+            <p className='destination-description'>
+              {destinations[2].description}
+            </p>
           ) : count == 3 && destinations ? (
-            <p>{destinations[3].description}</p>
+            <p className='destination-description'>
+              {destinations[3].description}
+            </p>
           ) : null}
         </div>
         <div>
           <div>
-            <p>AVG.DISTANCE</p>
+            <p className='avg-dis-heading'>AVG.DISTANCE</p>
             {count == 0 && destinations ? (
-              <p>{destinations[0].distance}</p>
+              <p className='avg-dis-data'>{destinations[0].distance}</p>
             ) : count == 1 && destinations ? (
-              <p>{destinations[1].distance}</p>
+              <p className='avg-dis-data'>{destinations[1].distance}</p>
             ) : count == 2 && destinations ? (
-              <p>{destinations[2].distance}</p>
+              <p className='avg-dis-data'>{destinations[2].distance}</p>
             ) : count == 3 && destinations ? (
-              <p>{destinations[3].distance}</p>
+              <p className='avg-dis-data'>{destinations[3].distance}</p>
             ) : null}
           </div>
           <div>
-            <p>EST. TRAVEL TIME</p>
+            <p className='est-time-heading'>EST. TRAVEL TIME</p>
             {count == 0 && destinations ? (
-              <p>{destinations[0].travel}</p>
+              <p className='est-time'>{destinations[0].travel}</p>
             ) : count == 1 && destinations ? (
-              <p>{destinations[1].travel}</p>
+              <p className='est-time'>{destinations[1].travel}</p>
             ) : count == 2 && destinations ? (
-              <p>{destinations[2].travel}</p>
+              <p className='est-time'>{destinations[2].travel}</p>
             ) : count == 3 && destinations ? (
-              <p>{destinations[3].travel}</p>
+              <p className='est-time'>{destinations[3].travel}</p>
             ) : null}
           </div>
         </div>
